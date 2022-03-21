66312e54d5f120e94009cdbb41a216e361f71f62
DylanLaptop <dylannicolet@hotmail.com>
2022-03-21 13:25:11 +0400

    Initial setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+
+const api = {
+  key: "5f826a580f1290aece38d4df9e0499eb",
+  base: "http://api.openweathermap.org/data/2.5"
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      hello
     </div>
   );
 }
